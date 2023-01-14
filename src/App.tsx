7c9cf0a26de477da72571bbc6215b510d73bffca
Alexander Martinovich <alexander_martinovich@msn.com>
2023-01-14 21:31:20 +0200

    feat: add basic garage data getter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.scss';
 
+const BASE_URL = 'http://127.0.0.1:3000';
+
+type TCars = {
+  name: string;
+  color: string;
+  id: number;
+}
+
 function App() {
+  const [garage, setGarage] = useState<TCars[]>([]);
+  const [totalCars, setTotalCars] = useState('0');
+
+  async function getGarage() {
+    const response = await fetch(`${BASE_URL}/garage?_limit=7`);
+    const totalCarsInHeader = response.headers.get('X-Total-Count');
+    if (totalCarsInHeader) setTotalCars(totalCarsInHeader);
+    const data = await response.json();
+    setGarage(data);
+  }
+
+  useEffect(() => {
+    getGarage();
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
@@ -11,7 +34,27 @@ function App() {
       </header>
       <div>
         Garage
+        <form>
+          <input type="text" name="" id="" />
+          <input type="color" name="" id="" />
+          <button type="button">create</button>
+        </form>
+        <form>
+          <input type="text" name="" id="" />
+          <input type="color" name="" id="" />
+          <button type="button">update</button>
+        </form>
+        <button type="button">race</button>
+        <button type="button">reset</button>
+        <button type="button">generate cars</button>
+
+        <div>Race track</div>
+        <div>Total cars from length: {garage.length}</div>
+        <div>Total cars from header: {totalCars}</div>
+        {garage.map((car) => <div style={{ color: car.color }}>{car.id} {car.name} {car.color}</div>)}
       </div>
+
+      <div>Score tab</div>
     </div>
   );
 }
