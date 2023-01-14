fd0570fe4519eb11c92a9abd399ad81dcfc5aa88
Alexander Martinovich <alexander_martinovich@msn.com>
2023-01-14 20:21:35 +0200

    init: start async-race task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        Header
+        <button type="button">to garage</button>
+        <button type="button">to winners</button>
       </header>
+      <div>
+        Garage
+      </div>
     </div>
   );
 }
